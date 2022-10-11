fc9169573cae7a8bd76ee20c2ddae2904088571a
Gabo6480 <gabominecraftero@gmail.com>
2022-10-11 15:27:23 -0500

    Refactored Mission handling, more compat.
    Refactored mission handling code so the common code is centralized.
    Added BREED mission type.
    Replaced "Material" setting on MINE & PLACE missions with "Item" for consistency with other mission types.
    Replaced "EntityType" with "Entity".
    FISH and ENCHANT missions can now select for specific Items.
    "Entity" and "Item" fields now use regex for matching, which allows more flexibility when selecting.

--- src/main/java/com/massivecraft/factions/missions/MissionHandler.java
+++ src/main/java/com/massivecraft/factions/missions/MissionHandler.java
@@ -2,10 +2,12 @@ package com.massivecraft.factions.missions;
 
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
 import com.massivecraft.factions.FactionsPlugin;
 import com.massivecraft.factions.util.CC;
 import com.massivecraft.factions.zcore.util.TL;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,12 +15,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTameEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MissionHandler implements Listener {
@@ -27,101 +34,106 @@ public class MissionHandler implements Listener {
      * @author Driftay
      */
 
-    private FactionsPlugin plugin;
+    static final String matchAnythingRegex = ".*";
+
+    private final FactionsPlugin plugin;
 
     public MissionHandler(FactionsPlugin plugin) {
         this.plugin = plugin;
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerTame(EntityTameEvent event) {
-        if (!(event.getOwner() instanceof Player)) {
+    public void onPlayerBreed(EntityBreedEvent e) {
+        if (!(e.getBreeder() instanceof Player)) {
             return;
         }
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer((Player) event.getOwner());
+        FPlayer fPlayer = FPlayers.getInstance().getByPlayer((Player) e.getBreeder());
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.TAME).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
-            if (!event.getEntityType().toString().equals(section.getConfigurationSection("Mission").getString("EntityType")) && !section.getConfigurationSection("Mission").getString("EntityType").equalsIgnoreCase("ALL")) {
-                continue;
-            }
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
-        }
+
+        handleMissionsOfType(fPlayer, MissionType.BREED, (mission, section) -> {
+            String entity = section.getString("Mission.Entity", matchAnythingRegex);
+            return e.getEntityType().toString().matches(entity);
+        });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onEntityDeath(EntityDeathEvent event) {
-        if (event.getEntity() == null || event.getEntity().getKiller() == null) {
+    public void onPlayerTame(EntityTameEvent e) {
+        if (!(e.getOwner() instanceof Player)) {
             return;
         }
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(event.getEntity().getKiller());
+        FPlayer fPlayer = FPlayers.getInstance().getByPlayer((Player) e.getOwner());
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.KILL).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
-            if (!event.getEntityType().toString().equals(section.getConfigurationSection("Mission").getString("EntityType"))) {
-                continue;
-            }
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
-        }
+
+        handleMissionsOfType(fPlayer, MissionType.TAME, (mission, section) -> {
+            String entity = section.getString("Mission.Entity", matchAnythingRegex);
+            return e.getEntityType().toString().matches(entity);
+        });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockBreak(BlockBreakEvent event) {
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(event.getPlayer());
+    public void onEntityDeath(EntityDeathEvent e) {
+        if (e.getEntity() == null || e.getEntity().getKiller() == null) {
+            return;
+        }
+        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getEntity().getKiller());
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.MINE).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
-            if (!event.getBlock().getType().toString().equals(section.getConfigurationSection("Mission").getString("Material"))) {
-                continue;
-            }
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
-        }
+
+        handleMissionsOfType(fPlayer, MissionType.KILL, (mission, section) -> {
+            String entity = section.getString("Mission.Entity", matchAnythingRegex);
+            return e.getEntityType().toString().matches(entity);
+        });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockPlace(BlockPlaceEvent event) {
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(event.getPlayer());
+    public void onBlockBreak(BlockBreakEvent e) {
+        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.PLACE).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
-            if (!event.getBlock().getType().toString().equals(section.getConfigurationSection("Mission").getString("Material"))) {
-                continue;
-            }
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
-        }
+
+        handleMissionsOfType(fPlayer, MissionType.MINE, (mission, section) -> {
+            String item = section.getString("Mission.Item", matchAnythingRegex);
+            return e.getBlock().getType().toString().matches(item);
+        });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerFish(PlayerFishEvent event) {
-        if (event.getState() != PlayerFishEvent.State.CAUGHT_FISH) {
-            return;
-        }
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(event.getPlayer());
+    public void onBlockPlace(BlockPlaceEvent e) {
+        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.FISH).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
+
+        handleMissionsOfType(fPlayer, MissionType.PLACE, (mission, section) -> {
+            String item = section.getString("Mission.Item", matchAnythingRegex);
+            return e.getBlockPlaced().getType().toString().matches(item);
+        });
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerFish(PlayerFishEvent e) {
+        if (e.getState() != PlayerFishEvent.State.CAUGHT_FISH) {
+            return;
         }
+        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
+        if (fPlayer == null) {
+            return;
+        }
+
+        handleMissionsOfType(fPlayer, MissionType.FISH, (mission, section) -> {
+            String item = section.getString("Mission.Item", matchAnythingRegex);
+            if(e.getCaught() instanceof Item) {
+                Item caughtItem = (Item) e.getCaught();
+                return caughtItem.getItemStack().getType().toString().matches(item);
+            }
+            return false;
+        });
     }
 
     @EventHandler
@@ -130,12 +142,11 @@ public class MissionHandler implements Listener {
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.ENCHANT).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
-        }
+
+        handleMissionsOfType(fPlayer, MissionType.ENCHANT, (mission, section) -> {
+            String item = section.getString("Mission.Item", matchAnythingRegex);
+            return e.getItem().getType().toString().matches(item);
+        });
     }
 
     @EventHandler
@@ -144,23 +155,33 @@ public class MissionHandler implements Listener {
         if (fPlayer == null) {
             return;
         }
-        List<Mission> missions = fPlayer.getFaction().getMissions().values().stream().filter(mission -> mission.getType() == MissionType.CONSUME).collect(Collectors.toList());
-        for (Mission mission2 : missions) {
-            ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions").getConfigurationSection(mission2.getName());
 
-            if (!e.getItem().toString().contains(section.getConfigurationSection("Mission").getString("Item")) && !section.getConfigurationSection("Mission").getString("Item").equalsIgnoreCase("ALL")) {
-                continue;
-            }
-            mission2.incrementProgress();
-            checkIfDone(fPlayer, mission2, section);
-        }
+        handleMissionsOfType(fPlayer, MissionType.CONSUME, (mission, section) -> {
+            String item = section.getString("Mission.Item", matchAnythingRegex);
+            return e.getItem().getType().toString().matches(item);
+        });
     }
 
-    private void checkIfDone(FPlayer fPlayer, Mission mission, ConfigurationSection section) {
-        if (mission.getProgress() < section.getConfigurationSection("Mission").getLong("Amount")) {
+    private void handleMissionsOfType(FPlayer fPlayer, MissionType missionType, BiFunction<Mission, ConfigurationSection, Boolean> missionConsumer){
+        fPlayer.getFaction().getMissions().values().stream()
+                .filter(mission -> mission.getType() == missionType)
+                .forEach(mission -> {
+                    ConfigurationSection section = plugin.getFileManager().getMissions().getConfig().getConfigurationSection("Missions." + mission.getName());
+                    if(missionConsumer.apply(mission, section)){
+                        mission.incrementProgress();
+                        checkIfDone(fPlayer, mission, section);
+                    }
+                });
+    }
+
+    private void checkIfDone(FPlayer fPlayer, Mission mission, @Nullable ConfigurationSection section) {
+        if(section == null)
+            return;
+
+        if (mission.getProgress() < section.getLong("Mission.Amount")) {
             return;
         }
-        for (String command : section.getConfigurationSection("Reward").getStringList("Commands")) {
+        for (String command : section.getStringList("Reward.Commands")) {
             FactionsPlugin.getInstance().getServer().dispatchCommand(FactionsPlugin.getInstance().getServer().getConsoleSender(), command.replace("%faction%", fPlayer.getFaction().getTag()).replace("%player%", fPlayer.getPlayer().getName()));
         }
         fPlayer.getFaction().getMissions().remove(mission.getName());
--- src/main/java/com/massivecraft/factions/missions/MissionType.java
+++ src/main/java/com/massivecraft/factions/missions/MissionType.java
@@ -8,7 +8,8 @@ public enum MissionType {
     FISH,
     TAME,
     ENCHANT,
-    CONSUME;
+    CONSUME,
+    BREED;
 
 
 
